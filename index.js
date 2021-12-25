54af0ede5356e985d93b7364b768acefd0609212
vaibhav chaudhari <chaudharivaibhav60@gmail.com>
2021-12-25 12:56:32 +0530

    nodechatapp deploy

---
+++ index.js
@@ -0,0 +1,20 @@
+//node server which handle socket io connection
+const io = require("socket.io")(8000, {
+  cors: {
+    origin: "*",
+  },
+});
+const users = {};
+io.on("connection", (socket) => {
+  socket.on("new-user.joined", (name) => {
+    console.log("New User", name);
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+});
